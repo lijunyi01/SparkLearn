962157af42663c01004814d26feb449bd7df78c9
lijunyi01 <lijunyi01@gmail.com>
2020-03-18 17:15:35 +0800

    modify

--- src/main/scala/cn/cimba/sparkstream/StructedStream.scala
+++ src/main/scala/cn/cimba/sparkstream/StructedStream.scala
@@ -8,26 +8,27 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  * Created by ljy on 2020/3/15.
  * ok
  */
-// spark-submit --class cn.cimba.sparkstream.StructedStream --master yarn --num-executors 8 --executor-cores 2 --deploy-mode client --driver-memory 1G ./SparkLearn.jar
+// spark-submit --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/root/log4j.properties" --class cn.cimba.sparkstream.StructedStream --master yarn --num-executors 8 --executor-cores 2 --deploy-mode client --driver-memory 1G ./SparkLearn.jar
 object StructedStream {
 
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession
+    val sparkSession = SparkSession
       .builder
       .appName("StructuredNetworkWordCount")
-//      .master("local[2]")
+      .master("local[2]")
       .getOrCreate()
 
     //初始化，创建sparkContext
-    val sc = spark.sparkContext
+    val sc = sparkSession.sparkContext
+//    sc.setLogLevel("INFO")
     //初始化，创建StreamingContext，batchDuration为5秒
     val ssc = new StreamingContext(sc, Seconds(5))
 
-    import spark.implicits._
+    import sparkSession.implicits._
 
     // Create DataFrame representing the stream of input lines from connection to localhost:9999
-    val lines = spark.readStream
+    val lines = sparkSession.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 9999)
