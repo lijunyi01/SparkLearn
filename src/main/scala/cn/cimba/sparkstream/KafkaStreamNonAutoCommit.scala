60d080409d9bc7a85d6bf2b5582d1393be6a8854
lijunyi01 <lijunyi01@gmail.com>
2020-03-21 16:02:50 +0800

    modify

--- src/main/scala/cn/cimba/sparkstream/KafkaStreamNonAutoCommit.scala
+++ src/main/scala/cn/cimba/sparkstream/KafkaStreamNonAutoCommit.scala
@@ -1,11 +1,12 @@
 package cn.cimba.sparkstream
 
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.dstream.DStream
-import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
@@ -13,13 +14,13 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  * ok
  */
 // spark-submit --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/root/log4j.properties" --class cn.cimba.sparkstream.StructedStream --master yarn --num-executors 8 --executor-cores 2 --deploy-mode client --driver-memory 1G ./SparkLearn.jar
-object KafkaStream {
+object KafkaStreamNonAutoCommit {
 
   def main(args: Array[String]): Unit = {
 
     val sparkSession = SparkSession
       .builder
-      .appName("KafkaWordCount")
+      .appName("KafkaWordCountNonAutoCommit")
       .master("local[2]")
       .getOrCreate()
 
@@ -34,9 +35,9 @@ object KafkaStream {
       "bootstrap.servers" -> "192.168.8.170:9092,192.168.8.171:9092,192.168.8.172:9092,192.168.8.173:9092",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> "group02",
+      "group.id" -> "group03",
       "auto.offset.reset" -> "latest",
-      "enable.auto.commit" -> (true: java.lang.Boolean)
+      "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
     // val topics = Array("topicA", "topicB")
@@ -50,19 +51,24 @@ object KafkaStream {
     // 流(stream)中的每个项都是 ConsumerRecord
 //    stream.map(record => (record.key, record.value))
 
-    val lines: DStream[String] = stream.map(record => (record.value))
+    stream.foreachRDD { rdd =>
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
 
-    //对DSteam进行操作，你操作这个抽象（代理，描述），就像操作一个本地的集合一样
-    //切分压平
-    val words: DStream[String] = lines.flatMap(_.split(" "))
-    //单词和一组合在一起
-    val wordAndOne: DStream[(String, Int)] = words.map((_, 1))
-    //聚合
-    val reduced: DStream[(String, Int)] = wordAndOne.reduceByKey(_+_)
-    //打印结果(Action)
-    reduced.print()
+      val lines: RDD[String] = rdd.map(record => (record.value))
 
+      //对RDD进行操作，你操作这个抽象（代理，描述），就像操作一个本地的集合一样
+      //切分压平
+      val words: RDD[String] = lines.flatMap(_.split(" "))
+      //单词和一组合在一起
+      val wordAndOne: RDD[(String, Int)] = words.map((_, 1))
+      //聚合
+      val reduced: RDD[(String, Int)] = wordAndOne.reduceByKey(_+_)
+      //打印结果(Action)
+      reduced.foreach(println(_))
 
+      // some time later, after outputs have completed
+      stream.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
+    }
 
     ssc.start()
     ssc.awaitTermination()
